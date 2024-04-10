d6050e24aca3b33210f92009cfef52df2cf55a59
sakhr <programmer98748@gmail.com>
2024-04-11 01:00:03 +0300

    projectt

--- project/website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'index.html')
-
-def project(request):
-    return render(request, 'project.html')
